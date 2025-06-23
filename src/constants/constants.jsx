a898d42b8dbc8696a7cca49fbf02a99446c477aa
sammyamajumdar <sammya512@gmail.com>
2025-06-23 15:18:32 +0530

    updated constants to reflect new endpoints

--- src/constants/constants.jsx
+++ src/constants/constants.jsx
@@ -1,3 +1,3 @@
-export const CHAT_ENDPOINT = "http://nasstarwebapp.hvchgccyh4gvgrd4.uksouth.azurecontainer.io:8000/get_response"
-export const GRAPH_ENDPOINT = "http://nasstarwebapp.hvchgccyh4gvgrd4.uksouth.azurecontainer.io:8000/weather_data"
-export const SUMMARY_ENDPOINT = "http://nasstarwebapp.hvchgccyh4gvgrd4.uksouth.azurecontainer.io:8000/get_response"
+export const CHAT_ENDPOINT = "https://nasstarappbackendv2.wonderfulmeadow-80a5de74.ukwest.azurecontainerapps.io/get_response"
+export const GRAPH_ENDPOINT = "https://nasstarappbackendv2.wonderfulmeadow-80a5de74.ukwest.azurecontainerapps.io/weather_data"
+export const SUMMARY_ENDPOINT = "https://nasstarappbackendv2.wonderfulmeadow-80a5de74.ukwest.azurecontainerapps.io/get_response"
